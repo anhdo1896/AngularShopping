5ab253710431ebab6122901d2d4d30dd64fae6b7
anhdo1896 <50945992+anhdo1896@users.noreply.github.com>
2021-06-29 22:24:37 +0700

    add interfaces

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import {HttpClientModule} from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
+//components
+import { AppComponent } from './app.component';
+import { ShopModule } from './shop/shop.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
